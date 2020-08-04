33765d5793cc434029afbf05e82d878b02317a51
liuzijian <liuzijian@gaohangip.com>
2020-08-04 12:18:29 +0800

    update

---
+++ src/api/test.js
@@ -0,0 +1,67 @@
+import request from '@/utils/request'
+const baseUrl = '/apiPro'
+
+class Test {
+  /**
+   * 消息中心
+   */
+  // 加载消息中心列表数据
+  static list(params) {
+    return request({
+      url: `${baseUrl}/index/test`,
+      method: 'get',
+      params: params
+    })
+  }
+
+  static info(data) {
+    return request({
+      url: `${baseUrl}/activity/info/` + data,
+      method: 'get'
+    })
+  }
+
+  /**
+   * 预览
+  */
+  static preView(data) {
+    return request({
+      url: `${baseUrl}/activity/preView/` + data,
+      method: 'get'
+    })
+  }
+
+  /**
+   * 下载
+  */
+  static upload(data) {
+    return request({
+      url: `${baseUrl}/activity/upload/` + data,
+      method: 'get'
+    })
+  }
+
+  /**
+   * 新增活动
+  */
+  static save(data) {
+    return request({
+      url: `${baseUrl}/activity/save`,
+      method: 'post',
+      data
+    })
+  }
+
+  /**
+   * 更新活动
+  */
+  static update(data) {
+    return request({
+      url: `${baseUrl}/activity/update`,
+      method: 'post',
+      data
+    })
+  }
+}
+
+export default Test
